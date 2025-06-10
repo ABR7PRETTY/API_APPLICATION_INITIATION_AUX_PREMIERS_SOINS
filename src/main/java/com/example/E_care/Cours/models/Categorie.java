5028f71d79c993a5fbc3d2a07865a1abbc30d87a
ABR7PRETTY <abr7pretty@gmail.com>
2025-06-10 14:51:28 +0000

    SuperAdmin Modification

--- src/main/java/com/example/E_care/Cours/models/Categorie.java
+++ src/main/java/com/example/E_care/Cours/models/Categorie.java
@@ -24,7 +24,7 @@ public class Categorie {
 
     @Lob  // Large Object Binary
     @Basic(fetch = FetchType.LAZY) // Permet d'optimiser la récupération des images
-    @Column(columnDefinition = "bytea") // Définit le type BLOB pour stocker de grandes images columnDefinition = "LONGBLOB"
+    @Column(nullable = false) // Définit le type BLOB pour stocker de grandes images columnDefinition = "LONGBLOB"
     private byte[] image;
 
 
